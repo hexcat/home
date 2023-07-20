a1e03cdd419d364e320aad88a900d8f044bae9ba
Artem Guzhva <21160542+hexcat@users.noreply.github.com>
2023-07-20 19:07:33 -0400

    Create setup-shell.zsh

---
+++ setup-shell.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+set -euo pipefail
+
+# Oh My Zsh
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# Powerlevel10k
+if [[ ! -f ~/.p10k.zsh ]]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
