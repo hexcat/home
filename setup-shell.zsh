ed449330e36e328615daea69f6e4c77676058c3b
Artem Guzhva <21160542+hexcat@users.noreply.github.com>
2023-07-20 19:51:58 -0400

    Add p10k.zsh

--- setup-shell.zsh
+++ setup-shell.zsh
@@ -10,4 +10,5 @@ fi
 # Powerlevel10k
 if [[ ! -f ~/.p10k.zsh ]]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+  cp "$(basename "$0")/p10k.zsh" ~/.p10k.zsh
 fi
