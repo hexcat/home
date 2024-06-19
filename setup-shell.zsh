b6fd3ba7c38adb56b1b506138311cf74eca394b3
Artem Guzhva <21160542+hexcat@users.noreply.github.com>
2024-06-19 13:24:54 +0000

    Link tmux.conf

--- setup-shell.zsh
+++ setup-shell.zsh
@@ -8,7 +8,7 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
 fi
 
 # Powerlevel10k
-if [[ ! -a ~/.p10k.zsh.pre-hexcat ]]; then
+if [[ ! -f ~/.p10k.zsh.pre-hexcat ]]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
   ln -s --backup --suffix=.pre-hexcat "$install_dir/p10k.zsh" ~/.p10k.zsh
 fi
@@ -22,4 +22,10 @@ fi
 # Zsh
 if [[ ! -f ~/.zshrc.pre-hexcat ]]; then
   ln -s --backup --suffix=.pre-hexcat "$install_dir/zshrc" ~/.zshrc
+fi
+
+# Tmux
+if [[ ! -f ~/.config/tmux/tmux.conf ]]; then
+  mkdir -p ~/.config/tmux
+  ls -s "$install_dir/tmux.conf" ~/.config/tmux/tmux.conf
 fi
